1cf6ced3cf2b9bba125f8f14423f998e242a19ec
Abdullah Al Mamun Apu <abdullah@futuretalk.ca>
2026-01-29 17:21:14 -0500

    Add timestamp formatter

---
+++ apps/web/src/lib/format.ts
@@ -0,0 +1,10 @@
+export function formatTimestamp(value: string): string {
+  if (!value) {
+    return "";
+  }
+  const date = new Date(value);
+  if (Number.isNaN(date.getTime())) {
+    return value;
+  }
+  return date.toLocaleString();
+}
